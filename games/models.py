cd646d1b6443774f96b05006708bfffb5c7ac5fc
Richard <richard.alexsander.guima@gmail.com>
2023-10-18 20:38:01 -0300

    feat: add tranlations

--- games/models.py
+++ games/models.py
@@ -4,27 +4,35 @@ from django.db import models
 
 
 class Championship(models.Model):
-    name = models.CharField()
-    image_url = models.CharField()
+    name = models.CharField(verbose_name='nome')
+    image_url = models.CharField(verbose_name='url da imagem')
 
     def __str__(self) -> str:
         return self.name
+
+    class Meta:
+        verbose_name = 'Campeonato'
+        verbose_name_plural = 'Campeonatos'
 
 
 class Team(models.Model):
-    name = models.CharField()
-    image_url = models.CharField()
+    name = models.CharField(verbose_name='nome')
+    image_url = models.CharField(verbose_name='url da imagem')
 
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        verbose_name = 'Time'
+        verbose_name_plural = 'Times'
+
 
 class Game(models.Model):
-    championship = models.ForeignKey(Championship, on_delete=models.CASCADE)
-    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_team')
-    visiting_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='visiting_team')
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+    championship = models.ForeignKey(Championship, on_delete=models.CASCADE, verbose_name='campeonato')
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_team', verbose_name='time mandante')
+    visiting_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='visiting_team', verbose_name='time visitante')
+    start_time = models.TimeField(verbose_name='inicio')
+    end_time = models.TimeField(verbose_name='final')
 
     def is_live(self) -> bool:
         return self.start_time < datetime.now().time() < self.end_time
@@ -35,16 +43,28 @@ class Game(models.Model):
     def __str__(self) -> str:
         return f'{self.home_team} X {self.visiting_team}'
 
+    class Meta:
+        verbose_name = 'Jogo'
+        verbose_name_plural = 'Jogos'
+
 
 class WatchButton(models.Model):
-    name = models.CharField()
-    name_in_page = models.CharField(blank=True)
+    name = models.CharField(verbose_name='nome')
+    name_in_page = models.CharField(blank=True, verbose_name='nome na página')
     url = models.CharField()
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, verbose_name='jogo')
 
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        verbose_name = 'Botão Assistir'
+        verbose_name_plural = 'Botões Assistir'
+
 
 class Configuration(models.Model):
-    header = models.CharField()
+    header = models.CharField(verbose_name='cabeçalho')
+
+    class Meta:
+        verbose_name = 'Configuração'
+        verbose_name_plural = 'Configurações'
