c925733b4df5dacb265d9414dfef1dd5919d6752
Richard <richard.alexsander.guima@gmail.com>
2023-11-03 22:09:10 -0300

    feat: now live games in top

--- games/views.py
+++ games/views.py
@@ -16,25 +16,46 @@ def index(request):
 
 def api(request):
     response = []
+    for game in Game.objects.all().order_by('-start_time'):
+        if game.is_live():
+            response.append(
+                {
+                    'championship': game.championship.name,
+                    'championship_image_url': game.championship.image_url,
+                    'home_team': game.home_team.name,
+                    'visiting_team': game.visiting_team.name,
+                    'home_team_image_url': game.home_team.image_url,
+                    'visiting_team_image_url': game.visiting_team.image_url,
+                    'start_time': f'{game.start_time.hour:>02}h{game.start_time.minute:>02}',
+                    'end_time': f'{game.end_time.hour:>02}h{game.end_time.minute:>02}',
+                    'is_live': game.is_live(),
+                    'is_finished': game.is_finished(),
+                    'buttons': [
+                        {'url': b.watch_button.url, 'name': b.name}
+                        for b in game.watchbuttonlabel_set.all()
+                    ],
+                },
+            )
     for game in Game.objects.all().order_by('start_time'):
-        response.append(
-            {
-                'championship': game.championship.name,
-                'championship_image_url': game.championship.image_url,
-                'home_team': game.home_team.name,
-                'visiting_team': game.visiting_team.name,
-                'home_team_image_url': game.home_team.image_url,
-                'visiting_team_image_url': game.visiting_team.image_url,
-                'start_time': f'{game.start_time.hour:>02}h{game.start_time.minute:>02}',
-                'end_time': f'{game.end_time.hour:>02}h{game.end_time.minute:>02}',
-                'is_live': game.is_live(),
-                'is_finished': game.is_finished(),
-                'buttons': [
-                    {'url': b.watch_button.url, 'name': b.name}
-                    for b in game.watchbuttonlabel_set.all()
-                ],
-            },
-        )
+        if not game.is_live():
+            response.append(
+                {
+                    'championship': game.championship.name,
+                    'championship_image_url': game.championship.image_url,
+                    'home_team': game.home_team.name,
+                    'visiting_team': game.visiting_team.name,
+                    'home_team_image_url': game.home_team.image_url,
+                    'visiting_team_image_url': game.visiting_team.image_url,
+                    'start_time': f'{game.start_time.hour:>02}h{game.start_time.minute:>02}',
+                    'end_time': f'{game.end_time.hour:>02}h{game.end_time.minute:>02}',
+                    'is_live': game.is_live(),
+                    'is_finished': game.is_finished(),
+                    'buttons': [
+                        {'url': b.watch_button.url, 'name': b.name}
+                        for b in game.watchbuttonlabel_set.all()
+                    ],
+                },
+            )
     response.append({'header': Configuration.objects.first().header})
     return HttpResponse(json.dumps(response))
 
