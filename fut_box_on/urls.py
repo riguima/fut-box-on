4dc66638572fe0ed0ca62e9b4478e08627dfae13
riguima <richard.alexsander.guima@gmail.com>
2024-02-13 02:02:40 +0000

    stable version

--- fut_box_on/urls.py
+++ fut_box_on/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('games/', include('games.urls')),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
--- games/admin.py
+++ games/admin.py
@@ -5,25 +5,32 @@ from . import models
 
 class WatchButtonLabelInline(admin.TabularInline):
     model = models.WatchButtonLabel
+    autocomplete_fields = ['watch_button']
+
+
+class ChampionshipAdmin(admin.ModelAdmin):
+    search_fields = ['name']
 
 
 class GameAdmin(admin.ModelAdmin):
     inlines = [WatchButtonLabelInline]
+    autocomplete_fields = ['championship', 'home_team', 'visiting_team']
 
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ['name']
     ordering = ['name']
+    search_fields = ['name']
 
 
 class WatchButtonAdmin(admin.ModelAdmin):
     list_display = ['name']
     ordering = ['name']
-
+    search_fields = ['name']
 
 
 admin.site.register(models.Team, TeamAdmin)
-admin.site.register(models.Championship)
+admin.site.register(models.Championship, ChampionshipAdmin)
 admin.site.register(models.Game, GameAdmin)
 admin.site.register(models.WatchButton, WatchButtonAdmin)
 admin.site.register(models.Configuration)
